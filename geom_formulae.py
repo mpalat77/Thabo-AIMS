d16af8301214fb1492db08a433c56400ca4f7298
mpalat77 <thabo@aims.edu.gh>
2014-08-31 17:26:53 +0000

    final edit

--- geom_formulae.py
+++ geom_formulae.py
@@ -5,7 +5,7 @@ from numbers import Number
 import math
 def circle_area(radius):
     """
-    Calculate the area of a circle from radius
+    Calculates the area of a circle from radius
     @param radius:length of radius of circle
     @return:area (units^2 from radius)
     >>> circle_area(8)
@@ -16,6 +16,7 @@ print (circle_area(8))
 
 def circle_circumference(radius):
     """
+    Calculates the circumference of a circle from its radius
     @param radius: length of radius of circle
     @return:circumference (units from radius of circle)
     >>> circle_circumference (8)
@@ -26,7 +27,7 @@ print (circle_circumference(8))
 
 def circle_circumference(diameter):
     """
-
+    Calculates the circumference of a circle from its diameter
     @param diameter: length of diameter of circle
     @return: circumference (units from diameter)
     >>> circle_circumference(16)
@@ -38,7 +39,7 @@ print(circle_circumference(16))
 
 def trapezium_area(a,b,height):
     """
-
+    Calculates the area of a trapezium from length of the upper and lower sides and height
     @param a: length of upper side or lower side of trapezium
     @param b: length of upper side or lower side of trapezium
     @param height: height of trapezium
@@ -51,7 +52,7 @@ print (trapezium_area(2,3,4))
 
 def triangle_area(base,height):
     """
-
+    Calculates the area of a triangle from its base length and height
     @param base: length of triangle base
     @param height: height of triangle
     @return: area (units^2 from base or height)
@@ -63,7 +64,7 @@ print (triangle_area(2,3))
 
 def triangle_area(side,adjacent_side,angle):
     """
-
+    Calculates the area of a triangle from the length of a side,adjacent side and the included angle
     @param side: length of any side
     @param adjacent_side: length of adjacent side
     @param angle: angle between the sides
@@ -76,6 +77,7 @@ print(triangle_area(4,5,60))
 
 def cube_area(side):
     """
+    Calculates the area of a cube from the length of its sides
     @param side: length of side of cube
     @return:area (unit^2 from side)
     >>> cube_area(4)
@@ -87,7 +89,7 @@ print (cube_area(4))
 
 def cylinder_volume(radius,height):
     """
-
+    Calculates the volume of a cylinder from its radius and height
     @param radius: length of radius of cylinder
     @param height: height of cylinder from one end to another
     @return:volume (units^3 units of radius or height)
@@ -99,7 +101,7 @@ print(cylinder_volume(3,6))
 
 def cone_volume(radius,height):
     """
-
+    Calculates the volume of a cone from its radius and height
     @param radius: length of radius of cone
     @param height: height of cone from base to top
     @return: volume(units^3 from height or radius
@@ -111,7 +113,7 @@ print (cone_volume(4,4))
 
 def sphere_volume(radius):
     """
-
+    Calculates the volume of a sphere from its radius
     @param radius: length of radius of sphere
     @return: volume (units^3 from radius)
     >>> sphere_volume(7)
@@ -122,7 +124,7 @@ print (sphere_volume(7))
 
 def rectangular_prism_volume(length,width,height):
     """
-
+    Calculates the volume of a rectangular prism from the length and width of its base and its height
     @param length: length of base of prism
     @param width: width of base of prism
     @param height: height of prism from base to top
@@ -135,6 +137,7 @@ print(rectangular_prism_volume(2,3,4))
 
 def rectangular_pyramid_volume(length,width,height):
     """
+    Calculates the volume of a rectangular prism from the length and width of its base and its height.
     @param length: length of the base of the pyramid
     @param width:  width of base of pyramid
     @param height: height of pyramid from base to top
@@ -147,7 +150,7 @@ print(rectangular_pyramid_volume(3,4,5))
 
 def square_pyramid_surface_area(side,slant_height):
     """
-
+    Calculates the surface area of a square pyramid from the length of the base side and the slant height
     @param side: length of the side of base
     @param slant_height: height of each triangular face from the base to the top
     @return: surface area(units^2 from side)
@@ -160,7 +163,7 @@ print(square_pyramid_surface_area(4,4))
 
 def square_pyramid_surface_area(side,height):
     """
-
+    Calculates the surface area of a square pyramid from the length of its base side and its height
     @param side: length of base side
     @param height: height from base to top
     @return:surface area(units^2 from side or height)
